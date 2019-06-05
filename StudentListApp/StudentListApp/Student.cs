635c6131fa342a18523e60e2f6c9e9fdc6a55df3
Jessie <michelle3208@163.com>
2019-06-05 11:29:26 +0800

    a student list demo

---
+++ StudentListApp/StudentListApp/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace StudentListApp
+{
+   public class Student
+    {
+        public int Code, Age, Image;
+        public string Nom, Prenom, Adresse, Infos, Classe;
+
+        public Student(int code, string nom, string prenom, int age, string adresse, string infos, int image, string classe)
+        {
+            this.Code = code;
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Age = age;
+            this.Adresse = adresse;
+            this.Infos = infos;
+            this.Image = image;
+            this.Classe = classe;
+        }
+    }
+}
---
+++ StudentListApp/StudentListApp/gridView_Adapter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace StudentListApp
+{
+    public class gridView_Adapter: BaseAdapter
+    {
+        List<Student> listStudents;
+        Activity a;
+
+
+        public gridView_Adapter(List<Student> listStudents, Activity a)
+        {
+            this.a = a;
+            this.listStudents = listStudents;
+        }
+        public override int Count => listStudents.Count;
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return position;
+        }
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var t = listStudents[position];
+            View v = convertView;
+            //Creation de la vue
+            v = a.LayoutInflater.Inflate(Resource.Layout.activity_image, null);
+            Button Fermer = v.FindViewById<Button>(Resource.Id.button1);
+            TextView name_tv = v.FindViewById<TextView>(Resource.Id.nametxt);
+            name_tv.Text = t.Nom;
+
+            v.FindViewById<ImageView>(Resource.Id.imageView1).SetImageResource(t.Image);
+            return v;
+        }
+    }
+}
